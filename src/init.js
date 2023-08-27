e056ade185a6899fac0334f42bd2fbaaa3887188
gangmin <minheek99@gmail.com>
2023-08-27 18:09:49 +0900

    Environment Variable : PORT

--- src/init.js
+++ src/init.js
@@ -1,13 +1,15 @@
+import "regenerator-runtime";
 import "dotenv/config";
+// require("dotenv").config();
 import "./db.js";
 import "./models/Video";
 import "./models/User";
 import "./models/Comment";
 import app from "./server.js";
 
-const PORT = 4000;
+const PORT = process.env.PORT || "4000";
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
-app.listen(4000, handleListening);
+app.listen(3000, "0.0.0.0", handleListening);
