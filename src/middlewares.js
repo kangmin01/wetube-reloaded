21be789c6e1b7ff67106e4f19b8374d02b39fc4a
gangmin <minheek99@gmail.com>
2023-08-29 20:46:47 +0900

    s3 uploads

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,21 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import { S3Client } from "@aws-sdk/client-s3";
+
+const s3 = new S3Client({
+  region: "ap-southeast-2",
+  credentials: {
+    apiVersion: "2023-08-29",
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET,
+  },
+});
+
+const multerUploader = multerS3({
+  s3: s3,
+  bucket: "wetube-mini",
+  acl: "public-read",
+});
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
@@ -30,6 +47,7 @@ export const avatarUpload = multer({
   limits: {
     fileSize: 3000000,
   },
+  storage: multerUploader,
 });
 
 export const videoUpload = multer({
@@ -37,4 +55,5 @@ export const videoUpload = multer({
   limits: {
     fileSize: 10000000,
   },
+  storage: multerUploader,
 });
